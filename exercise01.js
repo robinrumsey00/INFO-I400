a9f67580410e672524379dad4672d7661e537f8b
robinrumsey00 <89806399+robinrumsey00@users.noreply.github.com>
2021-09-08 16:23:11 -0400

    Add files via upload

---
+++ exercise01.js
@@ -0,0 +1,18 @@
+/* Welcome to the I400/590 Cross-Platform Mobile Programming Fall 2021
+
+This is a little exercise to try out some GitHub functions
+
+In this exercise, please 
+
+(1) Change the firstName, lastName, and date
+(2) Save the file
+(3) Commit the file back to your assignment repository (detailed steps please check the Canvas assignment "software setup")
+*/
+
+const firstName = "Christina";
+
+const lastName = "Chung";
+
+const date = "2021/08/23"
+
+console.log("First day at the I400/590 CPMP Fall 2020! My name is %s %s, today is %s", firstName, lastName, date);
